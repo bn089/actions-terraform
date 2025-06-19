cfaf99073ba0913ea21645e28cd6687288c40926
bn089 <nurkamilbubusara@gmail.com>
2025-06-19 21:04:28 +0000

    s3

--- main.tf
+++ main.tf
@@ -6,3 +6,13 @@ resource "aws_instance" "web" {
   ami           = "ami-09e6f87a47903347c"
   instance_type = "t2.micro"
 }
+
+
+terraform {
+  backend "s3" {
+    bucket = "kaizen-bubusaran"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+    use_lockfile = true
+  }
+}
